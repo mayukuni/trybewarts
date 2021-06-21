6c5de70e3394f9fadbddb618be8c45264a50801d
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-06-21 15:55:11 -0300

    adiciona melhorias

--- script.js
+++ script.js
@@ -35,3 +35,67 @@ function charCounter() {
 }
 
 textArea.addEventListener('input', charCounter);
+
+function stopDefAction(event) {
+  event.preventDefault();
+}
+
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const divInput = document.querySelector('.box-container');
+const inputEmail = document.getElementById('input-email');
+const label = document.querySelector('#label-houses');
+const pHouse = document.createElement('div');
+
+function updHouse() {
+  const select = document.querySelector('#house');
+  const { text } = select.options[select.selectedIndex];
+  pHouse.innerText = `Casa: ${text}`;
+  divInput.appendChild(pHouse);
+  divInput.removeChild(select);
+  divInput.removeChild(label);
+}
+
+function updFormName() {
+  const createP = document.createElement('div');
+  createP.innerText = `Nome: ${inputName.value} ${inputLastName.value}`;
+  divInput.appendChild(createP);
+  divInput.removeChild(inputName);
+  divInput.removeChild(inputLastName);
+}
+
+function updFormEmail() {
+  const pEmail = document.createElement('div');
+  pEmail.innerText = `Email: ${inputEmail.value}`;
+  divInput.appendChild(pEmail);
+  divInput.removeChild(inputEmail);
+}
+
+const frontEnd = document.getElementById('frontEnd');
+const backEnd = document.getElementById('backEnd');
+const fullStack = document.getElementById('fullStack');
+const family = document.querySelector('.family');
+const preferences = document.querySelector('#preferences');
+function updFamily() {
+  preferences.removeChild(family);
+  const pfamily = document.createElement('div');
+  preferences.appendChild(pfamily);
+  if (frontEnd.checked === true) {
+    pfamily.innerHTML = 'Família: FrontEnd';
+  } else if (backEnd.checked === true) {
+    pfamily.innerHTML = 'Família: BackEnd';
+  } else if (fullStack.checked === true) {
+    pfamily.innerHTML = 'Família: FullStack';
+  }
+}
+
+function updtForm() {
+  divInput.style.flexDirection = 'column';
+  updFormName();
+  updFormEmail();
+  updHouse();
+  updFamily();
+}
+
+submitBtn.addEventListener('click', stopDefAction, false);
+submitBtn.addEventListener('click', updtForm);
